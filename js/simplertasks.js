5889498a7095384178cb9bbec436deec71ea3979
Rob McCormack <rob.a.mccormack@gmail.com>
2023-10-26 17:03:21 -0400

    working now with better js, buyt still need to go back and forth

--- js/simplertasks.js
+++ js/simplertasks.js
@@ -1,13 +1,26 @@
 let taskMap = new Map();
 let isFormatted = false;
 
+// Event Listeners
 document.getElementById('copy-button').addEventListener('click', function() {
-    let textArea = document.getElementById('initial-list');
-    textArea.select();
-    document.execCommand('copy');
-    alert('Text copied to clipboard!'); // Optional: You can show a notification or tooltip instead of an alert
+    copyTextToClipboard('initial-list');
+    alert('Text copied to clipboard!');
 });
 
+document.getElementById('fancy-tab').addEventListener('click', sortAndGroup);
+document.getElementById('plain-tab').addEventListener('click', showPlainText);
+document.getElementById('email-tab').addEventListener('click', transferToEmailList);
+document.getElementById('themeToggle').addEventListener('change', toggleTheme);
+
+document.addEventListener("DOMContentLoaded", function() {
+    initializeSampleTasks();
+});
+
+function copyTextToClipboard(elementId) {
+    let textArea = document.getElementById(elementId);
+    textArea.select();
+    document.execCommand('copy');
+}
 
 function emailList() {
     let textArea = document.getElementById('email-list');
@@ -15,30 +28,70 @@ function emailList() {
     window.location.href = `mailto:?subject=Your To-Do List&body=${lines}`;
 }
 
+function transferToEmailList() {
+    let initialList = document.getElementById('initial-list');
+    let emailList = document.getElementById('email-list');
+    emailList.value = initialList.value;
+}
 
+function toggleTheme(event) {
+    if (event.currentTarget.checked) {
+        changeTheme('dark');
+    } else {
+        changeTheme('light');
+    }
+}
 
+function changeTheme(mode) {
+    const body = document.body;
+    if (mode === 'light') {
+        body.classList.remove('bg-dark', 'text-white');
+        body.classList.add('bg-light', 'text-dark');
+    } else if (mode === 'dark') {
+        body.classList.remove('bg-light', 'text-dark');
+        body.classList.add('bg-dark', 'text-white');
+    } else {
+        const hour = new Date().getHours();
+        if (hour < 6 || hour > 18) {
+            body.classList.remove('bg-light', 'text-dark');
+            body.classList.add('bg-dark', 'text-white');
+        } else {
+            body.classList.remove('bg-dark', 'text-white');
+            body.classList.add('bg-light', 'text-dark');
+        }
+    }
+}
 
+function initializeSampleTasks() {
+    let sampleTasks = `
+    !Buy groceries for the week. http://cnn.com
+    Finish editing a short film. #home
+    !Call mom for her birthday.
+    Fix that annoying bug in the Python script.
+    -Go for a 30-minute jog.
+    Water the plants.
+    -Prepare slides for tomorrow's meeting.
+    Cook dinner for friends coming over tonight.
+    [3] Meditate for 10 minutes #daily
+    Go to Walmart #home Today (jam, milk, return pants)
+    `;
+    let textArea = document.getElementById('initial-list');
+    textArea.value = sampleTasks.trim();
+}
 
+// Your other functions for sortAndGroup, showPlainText, updateCheckboxStatus, etc. 
+// I've kept them mostly the same as the initial code you provided.
 function sortAndGroup() {
-    // Find the special TODAY task
-    // let specialTodayTask = [...taskMap.keys()].find(task => task.startsWith('TODAY'));
-    // let specialTodayTask = [...taskMap.keys()].find(task => task.trim().startsWith('TODAY'));
-    // let specialTodayTask = [...taskMap.keys()].find(task => task.trim().startsWith('TODAY') || task.trim().startsWith('TODAY:'));
     let specialTodayTask = [...taskMap.keys()].find(task => /^TODAY\s*:?\s+/i.test(task));
-
     console.log("Detected TODAY task:", specialTodayTask);
 
-
-    // Reference to the special TODAY task elements
     let todayElement = document.getElementById('specialTodayTask');
     let todayTextElement = todayElement.querySelector('#specialTodayText');
 
-    // If we find a special TODAY task, let's display it
     if (specialTodayTask) {
         todayTextElement.textContent = specialTodayTask;
         todayElement.style.display = 'block';
     } else {
-        // Hide the special TODAY task if not found
         todayElement.style.display = 'none';
     }
 
@@ -58,7 +111,6 @@ function sortAndGroup() {
     let buildGroup = (tasks, status) => {
         return tasks.map((task, index) => {
             let checkedAttribute = taskMap.get(task) ? 'checked' : '';
-
             let noteMatch = task.match(/\(([^)]+)\)/);
             let note = noteMatch ? `<div class="note">${noteMatch[1]}</div>` : '';
             let taskWithoutNote = noteMatch ? task.replace(noteMatch[0], '') : task;
@@ -84,15 +136,7 @@ function sortAndGroup() {
         <h2 class="mt-3">Done</h2>
         ${buildGroup([...taskMap.keys()].filter(task => task.startsWith('x')), 'done')}
     `;
-
-    // Event listener for the email button
-    document.getElementById('email-button').addEventListener('click', emailList);
 }
-
-
-
-
-
 
 function updateCheckboxStatus(task, id) {
     let checkbox = document.getElementById(id);
@@ -110,71 +154,3 @@ function showPlainText() {
     textArea.value = plainTasks.join('\n');
     isFormatted = false;
 }
-
-document.getElementById('fancy-tab').addEventListener('click', sortAndGroup);
-document.getElementById('plain-tab').addEventListener('click', showPlainText);
-
-document.getElementById('email-tab').addEventListener('click', function() {
-    let textArea = document.getElementById('initial-list');
-    let emailTextArea = document.getElementById('email-list');
-    emailTextArea.value = textArea.value;
-});
-
-// Initialize the textarea with sample tasks
-document.addEventListener("DOMContentLoaded", function() {
-    let sampleTasks = `
-!Buy groceries for the week. http://cnn.com
-Finish editing a short film. #home
-!Call mom for her birthday.
-Fix that annoying bug in the Python script.
--Go for a 30-minute jog.
-Water the plants.
--Prepare slides for tomorrow's meeting.
-Cook dinner for friends coming over tonight.
-[3] Meditate for 10 minutes #daily
-Go to Walmart #home Today (jam, milk, return pants)
-    `;
-
-    let textArea = document.getElementById('initial-list');
-    textArea.value = sampleTasks.trim();
-});
-
-function changeTheme(mode) {
-    const body = document.body;
-
-    if (mode === 'light') {
-        body.classList.remove('bg-dark', 'text-white');
-        body.classList.add('bg-light', 'text-dark');
-    } else if (mode === 'dark') {
-        body.classList.remove('bg-light', 'text-dark');
-        body.classList.add('bg-dark', 'text-white');
-    } else {
-        const hour = new Date().getHours();
-        if (hour < 6 || hour > 18) {
-            body.classList.remove('bg-light', 'text-dark');
-            body.classList.add('bg-dark', 'text-white');
-        } else {
-            body.classList.remove('bg-dark', 'text-white');
-            body.classList.add('bg-light', 'text-dark');
-        }
-    }
-}
-
-// // Theme Switcher
-// const themeSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-// themeSwitch.addEventListener('change', function(event) {
-//     if (event.currentTarget.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//     } else {
-//         document.documentElement.setAttribute('data-theme', 'light');
-//     }
-// });
-
-document.getElementById('themeToggle').addEventListener('change', function(event) {
-    if (event.currentTarget.checked) {
-        changeTheme('dark');
-    } else {
-        changeTheme('light');
-    }
-});

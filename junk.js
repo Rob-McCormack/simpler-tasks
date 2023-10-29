18a3d47550d7921886db4ee6cfb4018cd03fcefe
Rob McCormack <rob.a.mccormack@gmail.com>
2023-10-29 08:39:36 -0400

    working pretty well, no #home font yet

--- junk.js
+++ junk.js
@@ -1,29 +1,9 @@
-function sortAndGroup() {
-    // ... [rest of the code remains unchanged]
-
-    let processTasks = (tasks) => {
-        return tasks.map(task => formatUsernamesInTask(task)).join('<br>');
-    };
-
-    sortedList.innerHTML = `
-        ${buildHeader("Must-Do Task for Today", "text-danger")}
-        ${processTasks([...taskMap.keys()].filter(task => task.toLowerCase().startsWith("t ")))}
-
-        ${buildHeader("Must-Do", "text-info")}
-        ${processTasks([...taskMap.keys()].filter(task => task.startsWith("! ") && !task.toLowerCase().startsWith("t ")))}
-
-        ${buildHeader("High Priority", "text-info")}
-        ${processTasks([...taskMap.keys()].filter(task => task.toLowerCase().startsWith("h ") && !task.toLowerCase().startsWith("t ") && !task.startsWith("! ")))}
-
-        ${buildHeader("Normal")}
-        ${processTasks([...taskMap.keys()].filter(task => !task.startsWith("! ") && !task.startsWith("- ") && !task.startsWith("x ") && !task.toLowerCase().startsWith("h ") && !task.toLowerCase().startsWith("l ") && !task.toLowerCase().startsWith("t ") && !task.startsWith("TITLE: ")))}
-
-        ${buildHeader("Low Priority")}
-        ${processTasks([...taskMap.keys()].filter(task => task.toLowerCase().startsWith("l ") && !task.toLowerCase().startsWith("t ")))}
-
-        ${buildHeader("Done")}
-        ${processTasks([...taskMap.keys()].filter(task => task.toLowerCase().startsWith("x ")))}
-    `;
-
-    // ... [rest of the code remains unchanged]
-}
+sortedList.innerHTML = `
+    ${mustDoHTML}
+    ${buildHeader("Today", "text-danger")}
+    ${processTasks([...taskMap.keys()].filter(task => task.toLowerCase().startsWith("t ") && task !== mustDoTask))}
+    
+    ${buildHeader("High Priority", "text-info")}
+    ${processTasks([...taskMap.keys()].filter(task => task.startsWith("h ") && task !== mustDoTask))}
+    // ... your other categories go here ...
+`;

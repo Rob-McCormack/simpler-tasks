046e2d07effcfe5fdc4379d2d76ac619074ca662
Rob McCormack <rob.a.mccormack@gmail.com>
2023-10-29 16:43:40 -0400

    TITLE to upper by use of template

--- js/simplertasks.js
+++ js/simplertasks.js
@@ -44,33 +44,73 @@ document.addEventListener("DOMContentLoaded", function () {
     transferToEmailList();
 });
 
+// document.getElementById("initial-list").addEventListener("input", function () {
+//     // Reconcile the taskMap with the textarea content
+//     reconcileTaskMapWithTextarea();
+//     let content = this.value;
+
+//     // Check if the first line starts with "TITLE:"
+//     if (content.startsWith("TITLE:")) {
+//         let title = content.split("\n")[0].replace("TITLE:", "").trim();
+//         let viewTab = document.getElementById("view");
+//         let titleElement = viewTab.querySelector("h3.title");
+//         if (!titleElement) {
+//             titleElement = document.createElement("h3");
+//             titleElement.classList.add("title");
+//             viewTab.prepend(titleElement);
+//         }
+//         titleElement.textContent = title;
+//     } else {
+//         let viewTab = document.getElementById("view");
+//         let titleElement = viewTab.querySelector("h3.title");
+//         if (titleElement) {
+//             titleElement.remove();
+//         }
+//     }
+
+//     reconcileTaskMapWithTextarea();
+//     displayTaskCounts();
+// });
+
+
 document.getElementById("initial-list").addEventListener("input", function () {
     // Reconcile the taskMap with the textarea content
     reconcileTaskMapWithTextarea();
+
     let content = this.value;
 
-    // Check if the first line starts with "TITLE:"
+    let viewTab = document.getElementById("view");
+    let titleTemplate = document.getElementById("title-template");
+
     if (content.startsWith("TITLE:")) {
         let title = content.split("\n")[0].replace("TITLE:", "").trim();
-        let viewTab = document.getElementById("view");
-        let titleElement = viewTab.querySelector("h3.title");
-        if (!titleElement) {
-            titleElement = document.createElement("h3");
-            titleElement.classList.add("title");
-            viewTab.prepend(titleElement);
-        }
+
+        // Clone the template
+        let titleClone = document.importNode(titleTemplate.content, true);
+
+        // Get the title element from the cloned content
+        let titleElement = titleClone.firstElementChild;
         titleElement.textContent = title;
+
+        // Check if an existing title element is already present, if so replace it, otherwise add it
+        let existingTitleElement = viewTab.querySelector(".text-uppercase");
+        if (existingTitleElement) {
+            viewTab.replaceChild(titleClone, existingTitleElement);
+        } else {
+            viewTab.prepend(titleClone);
+        }
     } else {
-        let viewTab = document.getElementById("view");
-        let titleElement = viewTab.querySelector("h3.title");
-        if (titleElement) {
-            titleElement.remove();
+        // If the content does not start with "TITLE:", remove the existing title element if present
+        let existingTitleElement = viewTab.querySelector(".text-uppercase");
+        if (existingTitleElement) {
+            existingTitleElement.remove();
         }
     }
 
     reconcileTaskMapWithTextarea();
     displayTaskCounts();
 });
+
 
 function displayTaskCounts() {
     const tasks = [...taskMap.keys()];
--- junk.js
+++ junk.js
@@ -1,9 +1,37 @@
-sortedList.innerHTML = `
-    ${mustDoHTML}
-    ${buildHeader("Today", "text-danger")}
-    ${processTasks([...taskMap.keys()].filter(task => task.toLowerCase().startsWith("t ") && task !== mustDoTask))}
-    
-    ${buildHeader("High Priority", "text-info")}
-    ${processTasks([...taskMap.keys()].filter(task => task.startsWith("h ") && task !== mustDoTask))}
-    // ... your other categories go here ...
-`;
+document.getElementById("initial-list").addEventListener("input", function () {
+    // Reconcile the taskMap with the textarea content
+    reconcileTaskMapWithTextarea();
+
+    let content = this.value;
+
+    let viewTab = document.getElementById("view");
+    let titleTemplate = document.getElementById("title-template");
+
+    if (content.startsWith("TITLE:")) {
+        let title = content.split("\n")[0].replace("TITLE:", "").trim();
+
+        // Clone the template
+        let titleClone = document.importNode(titleTemplate.content, true);
+
+        // Get the title element from the cloned content
+        let titleElement = titleClone.firstElementChild;
+        titleElement.textContent = title;
+
+        // Check if an existing title element is already present, if so replace it, otherwise add it
+        let existingTitleElement = viewTab.querySelector(".text-uppercase");
+        if (existingTitleElement) {
+            viewTab.replaceChild(titleClone, existingTitleElement);
+        } else {
+            viewTab.prepend(titleClone);
+        }
+    } else {
+        // If the content does not start with "TITLE:", remove the existing title element if present
+        let existingTitleElement = viewTab.querySelector(".text-uppercase");
+        if (existingTitleElement) {
+            existingTitleElement.remove();
+        }
+    }
+
+    reconcileTaskMapWithTextarea();
+    displayTaskCounts();
+});

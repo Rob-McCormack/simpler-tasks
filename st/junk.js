9c5538aa659dd58dcbc934deaa6c6cf07a5240b7
Rob McCormack <rob.a.mccormack@gmail.com>
2023-11-02 15:35:18 -0400

    extra line being deleted, and `happy` is not high priority

--- st/junk.js
+++ st/junk.js
@@ -1,12 +1,15 @@
-const specialChars = [
-    { char: "title", meaning: "title", sortOrder: 1 },
-    { char: "update", meaning: "update", sortOrder: 2 },
-    { char: "!", meaning: "must-do", sortOrder: 3 },
-    { char: "t", meaning: "today", sortOrder: 4 },
-    { char: "h", meaning: "high priority", sortOrder: 5 },
-    { char: "", meaning: "normal", sortOrder: 6 },
-    { char: "l", meaning: "low priority", sortOrder: 7 },
-    { char: "r", meaning: "recurring", sortOrder: 8 },
-    { char: "n", meaning: "note", sortOrder: 100 },
-    { char: "d", meaning: "done", sortOrder: 101 }
+OK, now some formatting things
+
+We want to set up constant:
+const colorClasses = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+    "muted"
 ];
+
--- st/st.js
+++ st/st.js
@@ -2,28 +2,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Define constants at the top
     const specialChars = [
-        { char: "title", meaning: "title", sortOrder: 1 },
-        { char: "update", meaning: "update", sortOrder: 2 },
-        { char: "!", meaning: "must-do", sortOrder: 3 },
-        { char: "t", meaning: "today", sortOrder: 4 },
-        { char: "h", meaning: "high priority", sortOrder: 5 },
-        { char: "", meaning: "normal", sortOrder: 6 }, // Make sure to include this
-        { char: "l", meaning: "low priority", sortOrder: 7 },
-        { char: "r", meaning: "recurring", sortOrder: 8 },
-        { char: "n", meaning: "note", sortOrder: 100 },
-        { char: "d", meaning: "done", sortOrder: 101 }
+        { char: "!", meaning: "must-do", sortOrder: 1 },
+        { char: "t", meaning: "today", sortOrder: 2 },
+        { char: "h", meaning: "high priority", sortOrder: 3 },
+        { char: "", meaning: "normal", sortOrder: 4 },
+        { char: "l", meaning: "low priority", sortOrder: 5 },
+        { char: "r", meaning: "recurring", sortOrder: 6 },
+        { char: "n", meaning: "note", sortOrder: 7 },
+        { char: "d", meaning: "done", sortOrder: 8 }
     ];
-
 
     const editTab = document.getElementById('edit-tab');
     const viewTab = document.getElementById('view-tab');
     const tasksTextArea = document.getElementById('tasks');
     const viewTasksDiv = document.getElementById('viewTasks');
 
+    // editTab.addEventListener('click', () => {
+    //     tasksTextArea.style.display = 'block';
+    //     viewTasksDiv.style.display = 'none';
+    // });
+
     editTab.addEventListener('click', () => {
+        // Retrieve the processed tasks as an array without empty lines
+        let tasksArray = convertTasksToJSON(tasksTextArea.value).map(taskObj => {
+            // Prefix the special character to the task content if it's not normal
+            const specialCharObj = specialChars.find(sc => sc.meaning === taskObj.type);
+            const prefix = specialCharObj && taskObj.type !== 'normal' ? specialCharObj.char + ' ' : '';
+            return prefix + taskObj.content;
+        });
+        // Join the array back into a string with new lines
+        const processedTasks = tasksArray.join('\n');
+        // Set the text area value to the processed tasks without empty lines
+        tasksTextArea.value = processedTasks;
+
+        // Display the correct area
         tasksTextArea.style.display = 'block';
         viewTasksDiv.style.display = 'none';
     });
+
 
     viewTab.addEventListener('click', () => {
         const tasks = tasksTextArea.value;
@@ -43,24 +59,29 @@ document.addEventListener('DOMContentLoaded', function () {
         autoExpandTextarea(this);
     });
 
+    // This should be removed since it's a duplicate of the code above
+    // viewTab.addEventListener('click', () => {
+    //     // ... (rest of your viewTab click event code)
+    //     // Update the tasks display
+    //     viewTasksDiv.innerHTML = convertJSONToHTML(tasksJSON);
+    //     tasksTextArea.style.display = 'none';
+    //     viewTasksDiv.style.display = 'block';
+    // });
+
     autoExpandTextarea(tasksTextArea);
 
-    function convertTasksToJSON(tasks) {
-        return tasks.split('\n').map(task => {
-            if (task.trim() !== '') {
-                let type = 'normal';
-                let content = task;
-                specialChars.forEach(sc => {
-                    if (task.startsWith(sc.char + ' ')) {
-                        type = sc.meaning;
-                        content = task.substring(sc.char.length).trim();
-                    }
-                });
-                return { type: type, content: content };
+    function convertTasksToJSON(text) {
+        return text.split('\n').filter(line => line.trim() !== '').map(line => {
+            // Check if the line starts with a special character followed by a whitespace
+            const specialChar = specialChars.find(sc => line.startsWith(sc.char + ' '));
+            if (specialChar) {
+                return { type: specialChar.meaning, content: line.slice(2).trim() };
+            } else {
+                return { type: 'normal', content: line };
             }
-            return null;
-        }).filter(t => t !== null); // Filter out any nulls that were added for empty lines
+        });
     }
+
 
     function convertJSONToHTML(json) {
         return json.map(task => `<div><strong>[${task.type}]</strong> ${task.content}</div>`).join('');
@@ -77,16 +98,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // const specialChars = [
-    //     { char: "title", meaning: "title", sortOrder: 1 },
-    //     { char: "update", meaning: "update", sortOrder: 2 },
-    //     { char: "!", meaning: "must-do", sortOrder: 3 },
-    //     { char: "t", meaning: "today", sortOrder: 4 },
-    //     { char: "h", meaning: "high priority", sortOrder: 5 },
-    //     { char: "", meaning: "normal", sortOrder: 6 },
-    //     { char: "l", meaning: "low priority", sortOrder: 7 },
-    //     { char: "r", meaning: "recurring", sortOrder: 8 },
-    //     { char: "n", meaning: "note", sortOrder: 100 },
-    //     { char: "d", meaning: "done", sortOrder: 101 }
-    // ];
 });

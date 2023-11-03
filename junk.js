5fee517f61f3b1b59f065ab5872b4db09bc966d3
Rob McCormack <rob.a.mccormack@gmail.com>
2023-11-03 09:32:29 -0400

    JSON format outputing correctly, without sorting

--- junk.js
+++ junk.js
@@ -1,8 +1,14 @@
-let processTasks = (tasks) => {
-    return tasks.map(task => {
-        task = formatUsernamesInTask(task);  // Existing function call
-        task = wrapSpecialCharacter(task);   // Existing function call
-        task = stMarkDown(task);  // New function call for stMarkDown processing
-        return task;
-    }).join('<br>');
-};
+function convertJSONToHTML(tasksJSON) {
+    let html = '';
+    for (const id in tasksJSON) {
+        if (tasksJSON.hasOwnProperty(id)) {
+            const task = tasksJSON[id];
+            html += `<div class="task">`;
+            html += `<div class="type">${task.type}</div>`;
+            html += `<div class="description">${task.description}</div>`;
+            // Add other task properties here
+            html += `</div>`;
+        }
+    }
+    return html;
+}
--- st/st.js
+++ st/st.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     const specialChars = [
         { char: "!", meaning: "must-do", sortOrder: -2 },
         { char: "t", meaning: "today", sortOrder: 2 },
@@ -13,13 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
         { char: "update", meaning: "update", sortOrder: -1 }
 
     ];
-
     const specialFormats = [
         { char: "@", formatStart: "<b>", formatEnd: "</b>" },
         { char: "#", formatStart: "<i>", formatEnd: "</i>" },
         { char: "+", formatStart: "<u>", formatEnd: "</u>" }
     ];
-
     const editTab = document.getElementById('edit-tab');
     const viewTab = document.getElementById('view-tab');
     const tasksTextArea = document.getElementById('tasks');
@@ -36,28 +33,22 @@ document.addEventListener('DOMContentLoaded', function () {
         viewTasksDiv.style.display = 'none';
     });
 
+
     viewTab.addEventListener('click', () => {
+        // This is assuming you have a textarea with an id of 'tasksTextArea'
         const tasks = tasksTextArea.value;
         let tasksJSON = convertTasksToJSON(tasks);
-        console.log(tasksJSON)
-        tasksJSON = sortTasks(tasksJSON);
+        console.log('Tasks in JSON format:', JSON.stringify(tasksJSON, null, 2));
+
+        // Sort tasks if the sortTasks function is available and working correctly
+        //  TODO:   tasksJSON = sortTasks(tasksJSON);
+
+        // Call convertJSONToHTML and set the innerHTML of viewTasksDiv
         viewTasksDiv.innerHTML = convertJSONToHTML(tasksJSON);
+
         tasksTextArea.style.display = 'none';
         viewTasksDiv.style.display = 'block';
     });
-
-    // Get the FAB element
-    const fab = document.getElementById('scrollFab');
-
-    // Function to show FAB when scrolling down
-    window.onscroll = function () {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            fab.classList.add('fab-visible');
-        } else {
-            fab.classList.remove('fab-visible');
-        }
-    };
-
 
 
     function applySpecialFormats(content) {
@@ -81,59 +72,40 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     autoExpandTextarea(tasksTextArea);
-
     function convertTasksToJSON(text) {
-        const linesSet = new Set(); // Initialize a new Set to store unique lines
-        return text.split('\n')
-            .filter(line => {
-                const trimmedLine = line.trim();
-                if (trimmedLine === '' || linesSet.has(trimmedLine)) {
-                    // Skip empty lines or duplicates
-                    return false;
-                }
-                linesSet.add(trimmedLine); // Add the line to the Set
-                return true;
-            })
-            .map(trimmedLine => {
-                const firstWord = trimmedLine.split(' ')[0];
-                const specialChar = specialChars.find(sc => sc.char.toLowerCase() === firstWord.toLowerCase());
-                if (specialChar) {
-                    return { type: specialChar.meaning, content: trimmedLine.substring(firstWord.length).trim() };
-                } else {
-                    return { type: 'normal', content: trimmedLine };
-                }
-            });
+        const lines = text.split('\n').filter(line => line.trim() !== ''); // Filter out empty lines
+        let tasksJSON = {};
+        let taskId = 1;
+
+        lines.forEach(line => {
+            const words = line.trim().split(' ');
+            const firstWord = words[0];
+            const specialChar = specialChars.find(sc => sc.char === firstWord);
+
+            let type, content;
+            if (specialChar) {
+                type = specialChar.meaning;
+                content = words.slice(1).join(' ');
+            } else {
+                type = 'normal';
+                content = line.trim();
+            }
+
+            tasksJSON[taskId] = { type: type, content: content };
+
+            // If it's an update, we can extract mentions, locations, projects
+            if (type === 'update') {
+                tasksJSON[taskId].mentions = words.filter(word => word.startsWith('@')).map(mention => mention.substring(1));
+                tasksJSON[taskId].locations = words.filter(word => word.startsWith('#')).map(location => location.substring(1));
+                tasksJSON[taskId].projects = words.filter(word => word.startsWith('+')).map(project => project.substring(1));
+            }
+
+            taskId++;
+        });
+
+        return tasksJSON;
     }
 
-
-
-    // function convertJSONToHTML(json) {
-    //     return json.map(task => {
-    //         const formattedContent = applySpecialFormats(task.content);
-    //         return `<div><strong>[${task.type}]</strong> ${formattedContent}</div>`;
-    //     }).join('');
-    // }
-
-
-    // function convertJSONToHTML(tasks) {
-    //     // First, create an object to hold the groups
-    //     const groupedTasks = tasks.reduce((acc, task) => {
-    //         // Initialize the array if this is the first task of this type
-    //         if (!acc[task.type]) {
-    //             acc[task.type] = [];
-    //         }
-    //         // Add the task to the appropriate type array
-    //         acc[task.type].push(task.content);
-    //         return acc;
-    //     }, {});
-
-    //     // Then, generate the HTML
-    //     return Object.entries(groupedTasks).map(([type, tasks]) => {
-    //         // Sort or manipulate tasks if needed
-    //         const tasksHTML = tasks.map(content => `<div>${applySpecialFormats(content)}</div>`).join('');
-    //         return `<div><strong>[${type}]</strong>${tasksHTML}</div>`;
-    //     }).join('');
-    // }
 
     function groupTasksByType(tasks) {
         return tasks.reduce((acc, task) => {
@@ -155,47 +127,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    // function convertJSONToHTML(json) {
-    //     let typeCounts = json.reduce((acc, task) => {
-    //         acc[task.type] = (acc[task.type] || 0) + 1;
-    //         return acc;
-    //     }, {});
-
-    //     let groupedHTML = json.reduce((acc, task) => {
-    //         if (!acc[task.type]) {
-    //             acc[task.type] = `<div><strong>[${task.type} <sup>${typeCounts[task.type]}</sup>]</strong></div>`;
-    //         }
-    //         const formattedContent = applySpecialFormats(task.content);
-    //         acc[task.type] += `<div>${formattedContent}</div>`;
-    //         return acc;
-    //     }, {});
-
-    //     return Object.values(groupedHTML).join('');
-    // }
-
-    function convertJSONToHTML(json) {
-        const typeCounts = countItems(json);
-        let totalItems = 0;
-
-        let groupedHTML = json.reduce((acc, task) => {
-            if (!acc[task.type]) {
-                acc[task.type] = `<div><strong>[${task.type} <sup>${typeCounts[task.type]}</sup>]</strong></div>`;
+    function convertJSONToHTML(tasksJSON) {
+        let html = '';
+        for (const id in tasksJSON) {
+            if (tasksJSON.hasOwnProperty(id)) {
+                const task = tasksJSON[id];
+                html += `<div class="task">`;
+                html += `<div class="type">${task.type}</div>`;
+                html += `<div class="content">${task.content}</div>`; // Changed from description to content
+                // Add other task properties here if needed
+                html += `</div>`;
             }
-            totalItems += 1;
-            const formattedContent = applySpecialFormats(task.content);
-            acc[task.type] += `<div>${formattedContent}</div>`;
-            return acc;
-        }, {});
-
-        document.getElementById('view-tab').innerHTML = `<span class="material-symbols-outlined fs-1">
-                  visibility
-                </span> View <sup>${totalItems}</sup>`;
-
-        return Object.values(groupedHTML).join('');
+        }
+        return html;
     }
-
-
-
 
 
 

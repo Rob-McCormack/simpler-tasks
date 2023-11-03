ded04de6095542881890e589897ccbb85f3c4219
Rob McCormack <rob.a.mccormack@gmail.com>
2023-11-03 08:02:29 -0400

    Preparing for Majoy JSON switch in code Made a folder called `old` to hold working copies

---
+++ st/old/junk.js
@@ -0,0 +1,42 @@
+OK, now some formatting things
+
+We want to set up constant:
+const colorClasses = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+    "muted"
+];
+
+
+const specialFormats = [
+    { char: "@", formatStart: "<b>", formatEnd: "</b>" },
+    { char: "#", formatStart: "<i>", formatEnd: "</i>" },
+    { char: "+", formatStart: "<u>", formatEnd: "</u>" }
+];
+
+// Later, in your convertJSONToHTML function, you'd use these formats:
+
+function applySpecialFormats(content) {
+    let formattedContent = content;
+    specialFormats.forEach(special => {
+        const regex = new RegExp(`\\${special.char}\\w+`, 'g'); // Match the special character followed by any word characters
+        formattedContent = formattedContent.replace(regex, match => {
+            // Wrap the matched text with the specified formatting
+            return `${special.formatStart}${match}${special.formatEnd}`;
+        });
+    });
+    return formattedContent;
+}
+
+function convertJSONToHTML(json) {
+    return json.map(task => {
+        const formattedContent = applySpecialFormats(task.content);
+        return `<div><strong>[${task.type}]</strong> ${formattedContent}</div>`;
+    }).join('');
+}

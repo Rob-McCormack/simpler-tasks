d73645f1436573d63be7aea900263510dc69a110
Rob McCormack <rob.a.mccormack@gmail.com>
2023-11-05 09:21:31 -0500

    improving local stor and  format

--- junk.js
+++ junk.js
@@ -1,10 +1,30 @@
-{
-  "highlight.regexes": {
-    "(^title\\s+.*)": {
-      "filterFileRegex": ".*\\.stasks$",
-        "decorations": [
-          { "backgroundColor": "#0000ff", "color": "#ffffff" }
-        ]
+<script>
+    // Function to automatically save content to localStorage
+  function autoSaveToLocalStorage() {
+        const content = document.getElementById('myTextarea').value;
+  localStorage.setItem('textareaContent', content);
+  console.log('Content saved:', content); // Console log for debugging
     }
-  }
-}
+
+  // Setup the autosave feature after the DOM is fully loaded
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('myTextarea').addEventListener('input', autoSaveToLocalStorage);
+
+  // Load content from localStorage when the page loads
+  const savedContent = localStorage.getItem('textareaContent');
+  if (savedContent) {
+    document.getElementById('myTextarea').value = savedContent;
+        }
+    });
+
+  // Service worker code
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => {
+        console.log('Service Worker registered!', reg);
+      })
+      .catch(err => {
+        console.error('Service Worker registration failed:', err);
+      });
+    }
+</script>
